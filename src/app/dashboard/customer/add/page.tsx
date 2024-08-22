e808205164fab474c58a00244dafe86ee9e7ee74
suyesh <pokharelsols98765@gmail.com>
2024-08-22 09:02:55 +0545

    form validate

--- src/app/dashboard/customer/add/page.tsx
+++ src/app/dashboard/customer/add/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import CustomInput from "../../../../component/CustomInput";
 import axios from "axios";
+import { boolean, number, object, string } from "yup";
 
 const Add = () => {
   const [firstName, setFirstName] = useState("");
@@ -13,19 +14,49 @@ const Add = () => {
   const [city, setCity] = useState("");
   const [street, setStreet] = useState("");
   const [zip, setZip] = useState("");
-  const [is_vendor, setIs_vendor]=useState(false)
+  const [is_vendor, setIs_vendor] = useState(false)
+  
+  let customerSchema = object({
+    firstName: string().required("firstname is required"),
+    lastName: string().required("lastname is required"),
+    email: string().email().required(),
+    phone: string().length(10),
+    province: string(),
+    city: string(),
+    street: string(),
+    zip: number().nullable(),
+    is_vendor: boolean().required(),
+  });
+   
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    let formData = {
+      firstName: e.target[0].value,
+      lastName: e.target[1].value,
+      email: e.target[2].value,
+      phone: e.target[3].value,
+      province: e.target[4].value,
+      city: e.target[5].value,
+      street: e.target[6].value,
+      zip: e.target[7].value,
+      is_vendor: e.target[8].value,
+    };
+    const isValid = await customerSchema.isValid(formData);
+    console.log(isValid)
+  }
+
+
   const token =
     "eyJpZCI6MTIsImZpcnN0X25hbWUiOiJiaGF0IiwibWlkZGxlX25hbWUiOm51bGwsImxhc3RfbmFtZSI6ImJoYXR0YXJhaSIsImVtYWlsIjoiYmhoYXR0dEBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCR3Nklrb1ZoVU5xdm91dUx0M0cyNXVPWW15bGpvVnR1L2NYSnd0aFFIVTlOYXpRUlFhRDJCaSIsInJvbGVfaWQiOjEsImNyZWF0ZWRfYXQiOiIyMDI0LTA3LTMxVDA2OjI3OjU1Ljc5NloiLCJ1cGRhdGVkX2F0IjoiMjAyNC0wNy0zMVQwNjoyNzo1NS43OTZaIiwiaWF0IjoxNzIyNDg5Nzg4fQ";
 
   
   const createCustomer = async () => {
-    const isVendor=""
     try {
       const response = await axios.post(
         "http://localhost:8000/customers",
         {
           name: `${firstName} ${lastName}`,
-          is_vender: isVendor,
+          is_vender: is_vendor,
           email: email,
           phone: phone,
           street_address: "brt",
@@ -40,7 +71,6 @@ const Add = () => {
           },
         }
       );
-      console.log(response);
     } catch (error) {
       console.error(error);
     }
@@ -78,6 +108,7 @@ const Add = () => {
       <form
         action="html"
         className="rounded-lg w-[600px] py-6 bg-white text-sm font-light flex justify-center flex-col items-center "
+        onSubmit={handleSubmit}
       >
         <CustomInput
           label="First Name"
